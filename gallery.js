1656ac74cd48227947f4acbb65ad06b06de5ca11
Unknown <ksaetern6@mail.csuchico.edu>
2017-04-25 22:11:13 -0700

    Finished lab
    created a random array value generator.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,15 @@
-
+var chico = ["Chico State", "Bear", "Moms", "Cinemark14","Ramen Place"];
+var current = "test";
 
 function shuffleGallery()
 {
-
+  var arrsize = chico.length;
+  var rand = Math.floor(Math.random()*arrsize);
+  var randvalue = chico[rand];
+  while(randvalue==current){
+    var rand = Math.floor(Math.random()*arrsize);
+    randvalue = chico[rand];
+  }
+  current = randvalue;
+  console.log(randvalue);
 }
